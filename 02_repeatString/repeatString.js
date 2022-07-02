649640dbd82c5089eb9cae82b90de30a952c1fb9
Kyle Rehak <kylerehak@gmail.com>
2022-07-02 20:26:04 +0800

    Complete exercise 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num = Math.floor(Math.random * 999 + 1)) {
+    const stringArray = [];
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        stringArray.push(string);
+    }
+    return stringArray.join('');
 };
 
 // Do not edit below this line
