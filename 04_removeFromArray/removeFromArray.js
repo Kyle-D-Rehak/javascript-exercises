7649607874b77b59178d1a169c9a33ef19122d95
Kyle Rehak <kylerehak@gmail.com>
2022-07-03 19:20:05 +0800

    Complete exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+
+
+const removeFromArray = function(array, ...args) {
+    function filterArray(item) {
+        for (let i = 0; i < args.length; i++) {
+            if (item === args[i]) {
+                return false;
+            } else if (i === (args.length - 1)) {
+                return true;
+            }
+        }
+    }
+    let result = array.filter(item => filterArray(item));
+    return result;
 
 };
 
