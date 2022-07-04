0f995923e90814a3336cb698d54b3b0522338763
Kyle Rehak <kylerehak@gmail.com>
2022-07-04 18:51:36 +0800

    Complete exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    let start, end;
+    if (a > b) {
+        start = b;
+        end = a;
+    } else {
+        start = a;
+        end = b;
+    }
+    let result = 0;
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
