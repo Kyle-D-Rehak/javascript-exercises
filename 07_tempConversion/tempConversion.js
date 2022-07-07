342237b75c53a1e7382c405db7c594e02bade0c4
Kyle Rehak <kylerehak@gmail.com>
2022-07-07 19:58:26 +0800

    Complete exercise 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+const result = ((fTemp - 32) * 5 / 9 );
+if (result % 1 === 0) {
+  return result
+} else return +result.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  const result = ((cTemp * 9 / 5) + 32);
+  if (result % 1 === 0) {
+    return result
+  } else return +result.toFixed(1);
 };
 
 // Do not edit below this line
