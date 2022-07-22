1fe53e34e0acffe44b7598d4ac4953e664f4bee3
Kyle Rehak <kylerehak@gmail.com>
2022-07-22 17:35:43 +0800

    Complete exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a, b) {
+  return +a + +b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return +a - +b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return (array.length == 0 ? 0 : array.reduce((sum, a) => sum + a));
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, a) => product * a, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for (let i = a; i > 0; i--) {
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
