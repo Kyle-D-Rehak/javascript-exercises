71e44f6ff87fc0c506b905601f8db6d7778293a1
Kyle Rehak <kylerehak@gmail.com>
2022-07-24 15:06:30 +0800

    Complete exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (+number < 0) return 'OOPS';
+    if (+number < 3) return 1;
+    let array = [];
+    array[0] = 1;
+    array[1] = 1;
 
+    for (let i = 2; i < +number; i++ ) {
+        array[i] = (array[i - 1] + array[i - 2]);
+    }
+    return array[array.length - 1]
 };
 
 // Do not edit below this line
