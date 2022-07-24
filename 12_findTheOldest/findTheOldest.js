903191cf64fb6b2383896ef5352b3bc257743d65
Kyle Rehak <kylerehak@gmail.com>
2022-07-24 18:39:21 +0800

    Complete exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const d = new Date();
+    people.forEach(person => {if(person.yearOfDeath == undefined) {person.yearOfDeath = d.getFullYear()}});
+    let sorted = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return sorted[0];
 };
 
 // Do not edit below this line
