a886832ad7fcc49866f6c64690b62fe31b81e2e8
Kyle Rehak <kylerehak@gmail.com>
2022-07-24 14:31:19 +0800

    Complete exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+let lowerCase = string.toLowerCase();
+let charArray = lowerCase.split('');
+let filteredArray = charArray.filter( item => /[a-z]/.test(item));
+return filteredArray.every((item, index, array) => item === array[array.length - index - 1]);
 };
 
 // Do not edit below this line
